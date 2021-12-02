c1f7bdbc484614b73e14768d4f7ac46de216da92
Mat2314 <mateusz.redzia@vstorm.co>
2021-12-02 18:18:25 +0100

    Removing extra code

--- main.py
+++ main.py
@@ -25,17 +25,12 @@ soup = BeautifulSoup(page.content, "html.parser")
 
 # Get all the data that is covered on the website
 article_div_content = soup.find("div", class_="articleContainer")
-    
-# Gather all the content together to put in template file
-main_website_content = ""
-for content in article_div_content:
-    main_website_content += str(content)
 
 # Render HTML template file content 
 templateLoader = jinja2.FileSystemLoader(searchpath="./")
 templateEnv = jinja2.Environment(loader=templateLoader)
 template = templateEnv.get_template(HTML_TEMPLATE_FILE)
-outputText = template.render(content=main_website_content)
+outputText = template.render(content=article_div_content)
 
 # Create HTML file with main content data
 with open(HTML_FILENAME, "w") as f:
